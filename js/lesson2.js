d40ab1555131ba29bf98dd88eaf7e6c342d5043d
Sergiy-Shevchenko <116056626+Sergiy-Shevchenko@users.noreply.github.com>
2023-02-26 13:01:58 +0200

    3zadacha

--- js/lesson2.js
+++ js/lesson2.js
@@ -172,71 +172,71 @@
 // console.log(capitalize("the quick brown fox"));
 
 
-ЗАДАЧА 9
-Напишіть функцію для роботи з колекцією навчаючих курсів courses:
+// ЗАДАЧА 9
+// Напишіть функцію для роботи з колекцією навчаючих курсів courses:
 
-addCourse(name) - добавляє курс у кінець колекції
-removeCourse(name) - видаляє курс із колекції
-updateCourse(oldName, newName)- змінює імя на нове
-const courses = ["HTML", "CSS", "JavaScript", "React", "PostgreSQL"];
+// addCourse(name) - добавляє курс у кінець колекції
+// removeCourse(name) - видаляє курс із колекції
+// updateCourse(oldName, newName)- змінює імя на нове
+// const courses = ["HTML", "CSS", "JavaScript", "React", "PostgreSQL"];
 
-// addCourse("Express"); // ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL', 'Express']
-// addCourse("CSS"); // 'У вас уже є такий курс'
-// removeCourse("React"); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'Express']
-// removeCourse("Vue"); // 'Курс з таким іменем не найдено'
-// updateCourse("Express", "NestJS"); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'NestJS']
-console.log(courses);
+// // addCourse("Express"); // ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL', 'Express']
+// // addCourse("CSS"); // 'У вас уже є такий курс'
+// // removeCourse("React"); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'Express']
+// // removeCourse("Vue"); // 'Курс з таким іменем не найдено'
+// // updateCourse("Express", "NestJS"); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'NestJS']
+// console.log(courses);
 
-ЗАДАЧА 1
-Напишіть скрипт,для обєкта user в послідовності:
+// ЗАДАЧА 1
+// Напишіть скрипт,для обєкта user в послідовності:
 
- добавляє поле mood із значенням 'happy'
-замінює значення hobby на 'skydiving'
-замінює значення  premium на false
-виводить отримане обєкта user в форматі ключ:значення використовуючи  Object.keys() и for...of
+//  добавляє поле mood із значенням 'happy'
+// замінює значення hobby на 'skydiving'
+// замінює значення  premium на false
+// виводить отримане обєкта user в форматі ключ:значення використовуючи  Object.keys() и for...of
 
- const user = {
-   name: "Mango",
-   age: 20,
-   hobby: "html",
-   premium: true,
-};
+//  const user = {
+//    name: "Mango",
+//    age: 20,
+//    hobby: "html",
+//    premium: true,
+// };
 
-ЗАДАЧА 2
-У вас є обєкт , у якому зберігаються зарплати нашої команди.
-Напишіть код , для підрахунку всіх зарплат і збережіть результат в змінні sum
-Повинно получитись 390, якщо обєкт salaries пустий , то результат буде 0
-ЩОБ МОЖНА БУЛО ПЕРЕВИКОРИСТОВУВАТИ ЛОГІКУ, ТОМУ ПИШЕМО ФУНКЦІЮ
-
-
- const salaries = {
-   John: 100,
-   Ann: 160,
-   Pete: 130,
- };
+// ЗАДАЧА 2
+// У вас є обєкт , у якому зберігаються зарплати нашої команди.
+// Напишіть код , для підрахунку всіх зарплат і збережіть результат в змінні sum
+// Повинно получитись 390, якщо обєкт salaries пустий , то результат буде 0
+// ЩОБ МОЖНА БУЛО ПЕРЕВИКОРИСТОВУВАТИ ЛОГІКУ, ТОМУ ПИШЕМО ФУНКЦІЮ
 
 
- ЗАДАЧА 3
-Створи обєкт calculator(калькулятор) з 3-ма методами:
-read()- запрошує 2 значення і зберігає їх як значення обєкта;
-sum() -повертає суму збережених значень; 
-mult() -перемножує значення збережені і повертає результат;
-
- const calculator = {
- a: null,
- b: null,
-
-  read() {},
-
-  sum() {},
-
-  mult() {},
-};
+//  const salaries = {
+//    John: 100,
+//    Ann: 160,
+//    Pete: 130,
+//  };
 
 
-calculator.read();
-console.log(`sum`, calculator.sum());
-console.log(`mult`, calculator.mult());
+//  ЗАДАЧА 3
+// Створи обєкт calculator(калькулятор) з 3-ма методами:
+// read()- запрошує 2 значення і зберігає їх як значення обєкта;
+// sum() -повертає суму збережених значень; 
+// mult() -перемножує значення збережені і повертає результат;
+
+//  const calculator = {
+//  a: null,
+//  b: null,
+
+//   read() {},
+
+//   sum() {},
+
+//   mult() {},
+// };
+
+
+// calculator.read();
+// console.log(`sum`, calculator.sum());
+// console.log(`mult`, calculator.mult());
 
 
 //1. Напиши скрипт, который для объекта user,
--- js/lesson4.js
+++ js/lesson4.js
@@ -84,3 +84,45 @@
 // };
 
 //refs.containerEl.style.backgroundColor = randomRgbColor();
+
+
+// ЗАДАЧА 3
+// 3.1 Є МАСИВ, ПОТРІБНО СТВОРИТИ РОЗМІТКУ ІЗ ТИХ КАРТОК через 
+
+const instruments = [{
+  id: 1,
+  img: "https://content.rozetka.com.ua/goods/images/big/13664457.jpg",
+  name: "Молоток",
+  price: 150
+},
+{
+  id: 2,
+  img: "https://machtz.com.ua/files/resized/products/dsc_2898_new.800x550.jpg",
+  name: "Перфоратор",
+  price: 3000
+},{
+  id: 3,
+  img: "https://content2.rozetka.com.ua/goods/images/big_tile/232653359.jpg",
+  name: "Рівень",
+  price: 2000
+}]
+
+
+
+const cardEl = document.querySelector('.js-list');
+
+function makeInstrumentsArray () {
+const instr = instruments.map(({id, img, name, price}) => {
+    return `
+    <li data-id="${id}">
+    <img src="${img}" alt="${name}" width='300'>
+    <h2>${name}</h2>
+    <p>${price}</p>
+    </li>    
+    `
+}).join(' ');
+cardEl.insertAdjacentHTML('beforeend', instr) 
+};
+makeInstrumentsArray();
+
+//3.2 ДОБАВТЕ КНОПКУ, ЧЕРЕЗ ЯКУ МИ КУПЛЯТИМЕМО ТОВАР
